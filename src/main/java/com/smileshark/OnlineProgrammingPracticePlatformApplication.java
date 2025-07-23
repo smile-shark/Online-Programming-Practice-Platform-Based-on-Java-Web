b625d579393dfaffc06e40e3beed7520e36fc743
smile-shark <488089248@qq.com>
2025-07-23 10:01:56 +0800

    项目框架搭建

---
+++ src/main/java/com/smileshark/OnlineProgrammingPracticePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.smileshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineProgrammingPracticePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineProgrammingPracticePlatformApplication.class, args);
+    }
+
+}
